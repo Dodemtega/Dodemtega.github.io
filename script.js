f44a320218a29e1dba3e14fdc5576b605e161410
Sin <98159246+Dodemtega@users.noreply.github.com>
2022-02-27 13:04:00 +0300

    Update script.js
    related to 404 page again

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+() => {
+let from404;
+window.onload = () => {
+    from404 = false;
+};
 if(document.getElementById('from') == "from404"){
-    var from404 = true;
+    from404 = true;
     document.getElementById('from404').innerHTML = "You Came This Page From 'Error 404' Page!"
-}
+}}
