3469846d36acc390bdc0a942491b8a343d7f968d
Sin <98159246+Dodemtega@users.noreply.github.com>
2022-02-27 13:05:15 +0300

    delete changes

--- script.js
+++
@@ -1,9 +0,0 @@
-() => {
-let from404;
-window.onload = () => {
-    from404 = false;
-};
-if(document.getElementById('from') == "from404"){
-    from404 = true;
-    document.getElementById('from404').innerHTML = "You Came This Page From 'Error 404' Page!"
-}}
